66f6c354787ccfc7011a317a49611e09e526c6f9
Roland Bewick <roland.bewick@gmail.com>
2022-11-17 19:40:44 +0700

    Increase static page generation timeout

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   experimental: { appDir: true },
+  staticPageGenerationTimeout: 60 * 5, // TODO: improve graph data calculation performance
 };
 
 module.exports = nextConfig;
