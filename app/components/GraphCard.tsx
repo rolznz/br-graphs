0fc6cb52035bbff1fd0e88fdf0ef6df84336b84e
Roland Bewick <roland.bewick@gmail.com>
2022-11-21 12:00:14 +0700

    Add simple fade out animation for graph cards

--- app/components/GraphCard.tsx
+++ app/components/GraphCard.tsx
@@ -1,6 +1,14 @@
 export function GraphCard({ children }: React.PropsWithChildren) {
   return (
-    <div className="shadow-lg p-4 rounded-xl bg-zinc-100 h-full md:min-h-[65vh] max-md:min-h-[40vh]">
+    <div className="shadow-lg p-4 rounded-xl bg-zinc-100 h-full md:min-h-[65vh] max-md:min-h-[40vh] relative">
+      <div
+        className="absolute w-full h-full p-4 -z-1 flex top-0 left-0"
+        style={{
+          animation: "fadeOut 3s forwards",
+        }}
+      >
+        <div className="flex-1 bg-slate-300 rounded-xl" />
+      </div>
       {children}
     </div>
   );
