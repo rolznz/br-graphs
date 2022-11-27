1368691be37c289835ea89f43f03f3463295d7c7
Roland Bewick <roland.bewick@gmail.com>
2022-11-27 17:19:59 +0700

    Add heroicons

--- app/components/ChartFilter.tsx
+++ app/components/ChartFilter.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { FunnelIcon, XMarkIcon } from "@heroicons/react/24/solid";
 import "chart.js/auto";
 import "chartjs-adapter-date-fns";
 import clsx from "clsx";
@@ -62,20 +63,30 @@ export function ChartFilter({
     [lastPurchaseDate, timeFormat]
   );
 
-  const [rangePickerOpen, setFilterMenuOpen] = React.useState(false);
-  const [selectionRange, setSelectionRange] = React.useState<Range>({
+  const [filterMenuOpen, setFilterMenuOpen] = React.useState(false);
+  const [dateRange, setDateRange] = React.useState<Range>({
     startDate,
     endDate,
     key: rangePickerKey,
   });
   const handleChangeDateRange = React.useCallback((ranges: RangeKeyDict) => {
-    setSelectionRange(ranges[rangePickerKey]);
+    setDateRange(ranges[rangePickerKey]);
   }, []);
+
+  const resetFilters = React.useCallback(() => {
+    setTimeFormat(defaultChartFilters.timeFormat);
+    setDateRange({
+      startDate,
+      endDate,
+      key: rangePickerKey,
+    });
+    setFilterMenuOpen(false);
+  }, [endDate, startDate]);
 
   return (
     <div className="relative w-full h-full">
-      {rangePickerOpen && (
-        <div className="absolute top-0 right-0 bg-secondary p-16 max-sm:p-4 rounded-lg flex flex-col items-start justify-center">
+      {filterMenuOpen && (
+        <div className="absolute top-0 right-0 bg-secondary p-16 py-4 max-sm:p-4 rounded-lg flex flex-col items-start justify-center">
           {showTimeFormat && (
             <>
               <p className="text-primary-content">View by</p>
@@ -97,40 +108,27 @@ export function ChartFilter({
           )}
           <p className="text-primary-content">Date range</p>
           <DateRangePicker
-            ranges={[selectionRange]}
+            ranges={[dateRange]}
             onChange={handleChangeDateRange}
             staticRanges={[]}
             inputRanges={[]}
           />
+          <button className="btn btn-primary mt-4" onClick={resetFilters}>
+            Reset Filters
+          </button>
         </div>
       )}
       <div className="absolute top-0 right-0 p-4">
         <label className="btn btn-circle swap swap-rotate">
           <input
             type="checkbox"
-            checked={rangePickerOpen}
+            checked={filterMenuOpen}
             onChange={(e) => setFilterMenuOpen(e.target.checked)}
           />
 
-          <svg
-            className="swap-off fill-current"
-            xmlns="http://www.w3.org/2000/svg"
-            width="32"
-            height="32"
-            viewBox="0 0 512 512"
-          >
-            <path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
-          </svg>
+          <FunnelIcon className="h-6 w-6 swap-off fill-current" />
 
-          <svg
-            className="swap-on fill-current"
-            xmlns="http://www.w3.org/2000/svg"
-            width="32"
-            height="32"
-            viewBox="0 0 512 512"
-          >
-            <polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
-          </svg>
+          <XMarkIcon className="h-6 w-6 swap-on fill-current" />
         </label>
       </div>
       {React.cloneElement(
@@ -138,9 +136,8 @@ export function ChartFilter({
         {
           filters: {
             timeFormat,
-            startDate:
-              selectionRange.startDate || defaultChartFilters.startDate,
-            endDate: selectionRange.endDate || defaultChartFilters.endDate,
+            startDate: dateRange.startDate || defaultChartFilters.startDate,
+            endDate: dateRange.endDate || defaultChartFilters.endDate,
           },
         }
       )}
