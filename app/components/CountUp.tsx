324849a9cd3475da6c87a1ef11f8d541dae061cc
Roland Bewick <roland.bewick@gmail.com>
2022-11-28 10:48:13 +0700

    Add react countup

---
+++ app/components/CountUp.tsx
@@ -0,0 +1,5 @@
+"use client";
+import ReactCountUp from "react-countup";
+
+const CountUp = ReactCountUp;
+export default CountUp;
--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,6 +1,7 @@
+import CountUp from "app/components/CountUp";
 import { ViewStatisticsButton } from "app/components/ViewStatisticsButton";
 import { format } from "date-fns";
-import { sortedPurchaseDates } from "lib/graphData";
+import { purchases, sortedPurchaseDates } from "lib/graphData";
 
 const formatHeroDate = (date: Date, includeYear = false) =>
   format(date, `MMM d${includeYear ? ", yyyy" : ""}`);
@@ -10,6 +11,9 @@ export function Hero() {
     <div className="hero min-h-screen">
       <div className="hero-content pb-16 text-center">
         <div className="max-w-lg">
+          <h1 className="mb-4 text-6xl text-primary-content font-bold tracking-wide h-16">
+            <CountUp end={purchases.length} duration={1} />
+          </h1>
           <h1 className="mb-4 text-4xl text-primary-content font-bold tracking-wide">
             Customer Purchases
           </h1>
