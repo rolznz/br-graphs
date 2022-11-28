a6e71ab6d7ddd1640f41330c8c2d140d916d32fe
Roland Bewick <roland.bewick@gmail.com>
2022-11-28 11:03:35 +0700

    Remove react-draggable

--- app/components/DragabbleComponent.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-import React from "react";
-import Draggable from "react-draggable";
-export function DraggableComponent({ children }: React.PropsWithChildren) {
-  return <Draggable>{children}</Draggable>;
-}
